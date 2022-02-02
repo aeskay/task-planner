822ff958a8efbbb63db58bd08c2f49045b9294aa
aeskay <alaladesambluez@gmail.com>
2022-02-02 14:19:22 +0900

    new

--- src/app/components/MainBody.js
+++ src/app/components/MainBody.js
@@ -15,24 +15,30 @@ function MainBody() {
                 <div><img src="/images/0.png" alt="Templates" className='img-fluid'/></div>
             </div>
 
-            <div className='section section-alt' style={{marginTop: 60}}>
-                <div className='sec-col'>
-                    <img src="/images/1.png" alt="Templates" className='img-fluid '/>
-                </div>
-                <div>
-                    <h4 className='task-title'>Ready-Made Templates</h4>
-                    <p className='subtitle'>You have access to free templates with sample tasks that allow you to get started easily.</p>
-                </div>
-            </div>
+            
 
-            <div className='section'  style={{marginTop: 60}}>
+            <div className='section section-alt'  style={{marginTop: 60}}>
+                
+                <div><img src="/images/3.png" alt="Templates" className='img-fluid'/></div>
                 <div>
                     <h4 className='task-title'>Track Your Completion</h4>
                     <p className='subtitle'>You can easily see the percentage of your work done. Mark each task or day as complete, delete task/day, add as many tasks as you want and keep track of all.</p>
                 </div>
-                <div><img src="/images/3.png" alt="Templates" className='img-fluid'/></div>
+            </div>
+
+            <div className='section' style={{marginTop: 60}}>
+            <div>
+                    <h4 className='task-title'>Ready-Made Templates</h4>
+                    <p className='subtitle'>You have access to free templates with sample tasks that allow you to get started easily.</p>
+                </div>
+                <div className='sec-col'>
+                    <img src="/images/1.png" alt="Templates" className='img-fluid '/>
+                </div>
+                
             </div>
         </div>
+
+        
     )
 }
 
